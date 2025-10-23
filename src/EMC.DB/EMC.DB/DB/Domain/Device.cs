37ea8b00e946e748c09a44cf90eeb698bfeda908
gony0811 <sean0811.kim@gmail.com>
2025-10-23 12:29:12 +0900

    EMC.DB device entity 추가 EMC.Data 프로젝트 추가

---
+++ src/EMC.DB/EMC.DB/DB/Domain/Device.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EMC.DB
+{
+
+    public class Device
+    {
+        [Key]
+        public int Id { get; private set; }
+
+        [Required]
+        [Index(nameof(Name), IsUnique = true)]
+        public string Name { get; set; }
+
+        public string Module { get; set; }
+
+        public DataType Type { get; set; }
+        public string TypeString { get; set; }
+
+
+    }
+}
